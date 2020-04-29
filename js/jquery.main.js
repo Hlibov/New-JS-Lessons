1f1413e011d78206a846769c7597cd7e89a905ed
Hlibov Hlib <gglebov777@gmail.com>
2020-04-29 11:27:22 +0300

    add codeadded code for lesson number 2

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -297,3 +297,385 @@ car["age"] > 5 ? (console.log("Need Repair"), car["needRepair"] = true) : car["n
 		}
 	}
 }());
+
+// Деструктурирующее присваивание. Задачи.
+(function () {
+	// 1. Используя rest оператор и деструктуризацию, создать функцию, которая принимает любое количество аргументов и возвращает объект, содержащий первый аргумент и массив из остатка:
+	function getObj(...arguments) {
+		let [firstEl, ...other] = arguments;
+		return {
+			first: firstEl,
+			other: other
+		};
+	}
+	let qwe = getObj(12, 34, 3, 0, -4);
+	console.log(qwe);
+
+	// 2. Организовать функцию getInfo, которая принимает объект вида
+	// { name: ...,  info: { employees: [...], partners: [ … ]  } }
+	// и выводит в консоль имя (если имени нет, показывать ‘Unknown’) и первые две компании из массива partners:
+	const organisation = {
+		name: "p2H",
+		info: {
+			employees: [2000, 50000, 1000],
+			partners: ["sony", "mazda", "nikon"]
+		}
+	}
+
+	function getInfo(objDate) {
+		const {
+			name = "Unknown", info: {
+				partners: [first, second]
+			}
+		} = objDate;
+		return {
+			Name: name,
+			Partners: [first, second]
+		};
+	}
+	let info = getInfo(organisation);
+	console.log(info);
+}());
+
+// Функции стрелки. Задачи.
+(function () {
+	// 1. Переделать функцию с использованием функции-стрелки (в методе reduce тоже использовать arrow function):
+	function sum() {
+		const params = Array.prototype.slice.call(arguments);
+
+		if (!params.length) return 0;
+
+		return params.reduce(function (prev, next) {
+			return prev + next;
+		});
+	}
+
+	const newSum = (...arguments) => arguments.reduce(function (prev, next) {
+		return prev + next
+	});
+	let res = newSum(1, 2, 3, 4);
+	console.log(res);
+}());
+
+// Функции высшего порядка. Задачи.
+(function () {
+	// 1.  Создать две функции и дать им осмысленные названия:
+	// - первая функция принимает массив и колбэк (одна для всех вызовов)
+	// - вторая функция (колбэк) обрабатывает каждый элемент массива (для каждого вызова свой callback)
+
+	// Первая функция возвращает строку “New value: ” и результат обработки:
+
+	// firstFunc([‘my’, ‘name’, ‘is’, ‘Trinity’], handler1) → “New value: MyNameIsTrinity”
+	// firstFunc([10, 20, 30], handler2) → “New value: 100, 200, 300,”
+	// firstFunc([{age: 45, name: ‘Jhon’}, {age: 20, name: ‘Aaron’}], handler3) →
+	// “New value: Jhon is 45, Aaron is 20,”
+	// firstFunc([‘abc’, ‘123’], handler4) → “New value: cba, 321,” // строки инвертируются
+
+	// Подсказка: secondFunc должна быть представлена функцией, которая принимает
+	// один аргумент (каждый элемент массива) и возвращает результат его обработки
+	function getRes(arrDate, handler) {
+		if (!Array.isArray(arrDate)) {
+			console.error("wrong data writing style");
+		} else {
+			return handler(arrDate);
+		}
+	}
+
+	// коллбек ф-я для преобразования эл. массива в одну строку
+	const getItemsPlus = (arr) => {
+		let res = "New value:";
+		for (let item of arr) {
+			res += " " + item;
+		}
+		return res;
+	}
+
+	// коллбек ф-я для умножения эл. массива и преобразования в одну строку
+	const getItemsMulti = (arr) => {
+		let res = "New value:";
+		for (let item of arr) {
+			res += " " + item * 10 + ",";
+		}
+		return res;
+	}
+
+	// коллбек ф-я для обработки данных из объектов и преобразование этих данных в одну строку
+	const getItemsObj = (arr) => {
+		let res = "New value:";
+		for (let item of arr) {
+			res += " " + item.name + " is " + item.age + ",";
+		}
+		return res;
+	}
+
+	let getResAll = getRes([{
+		age: 45,
+		name: "Jhon"
+	}, {
+		age: 20,
+		name: "Aaron"
+	}], getItemsObj);
+	console.log(getResAll);
+
+	// 2. Написать аналог метода every. Создайте функцию every, она должна принимать первым аргументом массив чисел (обязательно проверьте что передан массив) вторым аргументом callback (обязательно проверьте что передана функция)
+	// функция должна возвращать true или false в зависимости от результата вызова callback (проверить число больше 5). Callback  должен принимать один элемент массива, его индекс в массиве и весь массив.
+
+	function analogEvery(arrNumbers, func) {
+		if (!Array.isArray(arrNumbers) || typeof func != "function") {
+			console.error("wrong data writing style");
+		} else {
+			for (item of arrNumbers) {
+				let resBool = func(item);
+				if (!resBool) {
+					return false;
+				} else {
+					return true;
+				}
+			}
+		}
+	}
+
+	analogEveryCallback = (value) => {
+		return value > 5;
+		console.log(res);
+	};
+
+	let fff = analogEvery([3, 8, -5, -9, 1, 10], analogEveryCallback);
+	console.log(fff);
+}());
+
+// Перебирающие методы. Задачи.
+(function () {
+	// 1. На основе массива [1,2,3,5,8,9,10] сформировать новый массив,
+	// каждый элемент которого будет хранить информацию о числе и его четности:
+	// [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
+	let arr = [1, 2, 3, 5, 8, 9, 10],
+		newArr = arr.map(function (item) {
+			let obj = {
+				digit: item,
+				odd: null
+			};
+			if (item % 2 == 0) {
+				obj.odd = false;
+				return obj
+			} else {
+				obj.odd = true;
+				return obj
+			}
+		});
+	console.log(newArr);
+
+	//  2. Проверить, содержит ли массив [12, 4, 50, 1, 0, 18, 40] элементы, равные нулю. Если да - вернуть false.
+	let arr2 = [12, 4, 50, 1, 0, 18, 40];
+	let newArr2 = arr2.some(function (item) {
+		if (item == 0) {
+			return false
+		}
+	});
+	console.log(newArr2);
+
+	//  3. Проверить, содержит ли массив ['yes', 'hello', 'no', 'easycode', 'what'] хотя бы одно слово длиной больше 3х букв. Если да - вернуть true
+	let arr3 = ['yes', 'hello', 'no', 'easycode', 'what'];
+	let newArr3 = arr3.some(function (item) {
+		return item.length > 3;
+	});
+	console.log(newArr3);
+
+	// 4. Дан массив объектов, где каждый объект содержит информацию о букве и месте её положения в строке {буква: “a”, позиция_в_предложении: 1}:
+
+	// [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2},
+	// {char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0},
+	// {char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}]
+
+	// Напишите функцию, которая из элементов массива соберет и вернёт
+	// строку, основываясь на index каждой буквы. Например:
+	// [{char:"H",index:0}, {char:"i",index: 1}, {char:"!",index:2}] → “Hi!”
+
+	// Подсказка: вначале отсортируйте массив по index, затем используйте reduce() для построения
+	// строки
+	let arr4 = [{
+			char: "a",
+			index: 12
+		}, {
+			char: "w",
+			index: 8
+		}, {
+			char: "Y",
+			index: 10
+		}, {
+			char: "p",
+			index: 3
+		}, {
+			char: "p",
+			index: 2
+		},
+		{
+			char: "N",
+			index: 6
+		}, {
+			char: " ",
+			index: 5
+		}, {
+			char: "y",
+			index: 4
+		}, {
+			char: "r",
+			index: 13
+		}, {
+			char: "H",
+			index: 0
+		},
+		{
+			char: "e",
+			index: 11
+		}, {
+			char: "a",
+			index: 1
+		}, {
+			char: " ",
+			index: 9
+		}, {
+			char: "!",
+			index: 14
+		}, {
+			char: "e",
+			index: 7
+		}
+	]
+
+	let newArr4 = [];
+	arr4.forEach(function (item, ind, arr) {
+		for (let i = 0; i < arr.length; i++) {
+			if (item.index == i) {
+				newArr4[i] = item;
+			}
+		}
+	});
+	console.log(newArr4);
+	newArr4 = newArr4.map(function (item) {
+		return item.char;
+	});
+	console.log(newArr4);
+
+	newArr4 = newArr4.reduce(function (acc, val) {
+		return acc + val;
+	});
+	console.log(newArr4);
+}());
+
+// Метод Sort. Задачи.
+(function () {
+	// 1. Отсортируйте массив массивов так, чтобы вначале располагались наименьшие массивы (размер массива определяется его длиной): [  [14, 45],  [1],  ['a', 'c', 'd']  ] → [ [1], [14, 45], ['a', 'c', 'd'] ]
+	let arrMain = [
+		[14, 45],
+		[1],
+		['a', 'c', 'd']
+	];
+	arrMain.sort(function (previous, next) {
+		return previous.length - next.length;
+	});
+	console.log(arrMain);
+
+	// 2. Есть массив объектов:
+	// [
+	// 	{cpu: 'intel', info: {cores:2, сache: 3}},
+	// 	{cpu: 'intel', info: {cores:4, сache: 4}},
+	// 	{cpu: 'amd', info: {cores:1, сache: 1}},
+	// 	{cpu: 'intel', info: {cores:3, сache: 2}},
+	// 	{cpu: 'amd', info: {cores:4, сache: 2}}
+	// ]
+
+	// Отсортировать их по возрастающему количеству ядер (cores).
+
+	let arrBig = [{
+			cpu: 'intel',
+			info: {
+				cores: 2,
+				сache: 3
+			}
+		},
+		{
+			cpu: 'intel',
+			info: {
+				cores: 4,
+				сache: 4
+			}
+		},
+		{
+			cpu: 'amd',
+			info: {
+				cores: 1,
+				сache: 1
+			}
+		},
+		{
+			cpu: 'intel',
+			info: {
+				cores: 3,
+				сache: 2
+			}
+		},
+		{
+			cpu: 'amd',
+			info: {
+				cores: 4,
+				сache: 2
+			}
+		}
+	]
+	arrBig.sort(function (previous, next) {
+		return previous.info.cores - next.info.cores;
+	});
+	console.log(arrBig);
+
+	// 3. Создать функцию, которая будет принимать массив продуктов и две цены. Функция должна вернуть все продукты, цена которых находится в указанном диапазоне, и сортировать от дешевых к дорогим:
+
+	// let products = [
+	// 	{title: 'prod1', price: 5.2}, {title: 'prod2', price: 0.18},
+	// 	{title: 'prod3', price: 15}, {title: 'prod4', price: 25},
+	// 	{title: 'prod5', price: 18.9}, {title: 'prod6', price: 8},
+	// 	{title: 'prod7', price: 19}, {title: 'prod8', price: 63}
+	// ];
+
+	// filterCollection(products, 15, 30) → [{...price: 15}, {...price: 18.9}, {...price: 19}, {...price: 25}]
+	let products = [{
+			title: 'prod1',
+			price: 5.2
+		}, {
+			title: 'prod2',
+			price: 0.18
+		},
+		{
+			title: 'prod3',
+			price: 15
+		}, {
+			title: 'prod4',
+			price: 25
+		},
+		{
+			title: 'prod5',
+			price: 18.9
+		}, {
+			title: 'prod6',
+			price: 8
+		},
+		{
+			title: 'prod7',
+			price: 19
+		}, {
+			title: 'prod8',
+			price: 63
+		}
+	];
+
+	function getPrice(array, minPrice, maxPrice) {
+		let newArrPrice = array.filter(function (item) {
+			return minPrice < item.price && item.price < maxPrice;
+		});
+		return newArrPrice.sort(function (prev, next) {
+			return prev.price - next.price;
+		});
+	}
+	let listPrice = getPrice(products, 1, 20);
+	console.log(listPrice);
+}());
